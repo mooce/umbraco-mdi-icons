f3746edb14033b772b386c3aa59490fedfec2267
Dacre <enquiries@mooce.co.nz>
2018-07-12 22:47:18 +1200

    injecting asset service to controller, loading mdi css

--- mdiicons.controller.js
+++ mdiicons.controller.js
@@ -1,5 +1,7 @@
 angular.module("umbraco")
     .controller("Mooce.MdiIconPicker",
-    function () {
+    function ($scope,assetsService) {
         
+        //load in mdi css
+        assetsService.loadCss("/App_Plugins/MooceMdiIconPicker/css/materialdesignicons.css");
     });
